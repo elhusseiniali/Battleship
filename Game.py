63ace0a8eac9a3d6b2cef54aafca3753995c43b1
elhusseiniali <36609514+elhusseiniali@users.noreply.github.com>
2019-03-26 01:15:42 +0200

    Create Game.py

---
+++ Game.py
@@ -0,0 +1,99 @@
+import numpy as np
+ROWS = 10
+COLUMNS = 10
+# TO-DO: Properly inherit Game.rows and Game.columns
+# for Player.board
+
+
+class Game:
+    def __init__(self):
+        self.rows = ROWS
+        self.columns = COLUMNS
+
+        self.players = []
+
+    def set_players(self):
+        """
+        Function: set_players
+        Output: A list of 2 Player objects whose names are set by user input.
+        """
+        for _ in range(2):
+            name = input("Please enter your name: ")
+            self.players.append(Player(name))
+        return None
+
+    def allowed_ships(self):
+        """
+        Function: allowed_ships
+        Output: 5 types of ships based on Milton Bradley's version
+                of the rules of the game.
+        Reference: https://en.m.wikipedia.org/wiki/Battleship_(game)
+        """
+        ships = {
+            "Carrier": {"Length": 5, "Quantity": 1},
+            "Battleship": {"Length": 4, "Quantity": 1},
+            "Cruiser": {"Length": 3, "Quantity": 1},
+            "Submarine": {"Length": 3, "Quantity": 1},
+            "Destroyer": {"Length": 2, "Quantity": 1}
+        }
+        return ships
+
+
+class Player(Game):
+    def __init__(self, name):
+        self.name = name
+
+        self.fleet = Game.allowed_ships(Game)
+        for ship in self.fleet:
+            self.fleet[ship]["Sunk"] = 0
+
+        self.board = np.zeros((ROWS, COLUMNS), dtype=np.int8)
+
+    def set_board(self):
+        print("Your fleet currently has: ", self.fleet)
+        choice = input("Type 'yes' if you would like to randomly populate"
+                       "your board, or anything else to populate it yourself:"
+                       " ")
+
+        for ship in self.fleet:
+            print("We will place your {}, of size {}."
+                  .format(ship, self.fleet[ship]['Length']))
+            if choice.lower() == 'yes':
+                pass
+            else:
+                occupied = False
+                while not occupied:
+                    row = -1
+                    while not (0 <= row <= 9):
+                        try:
+                            row = int(input("Please enter the row you want"
+                                            "this ship to start in: "))
+                        except ValueError:
+                            pass
+
+                    column = -1
+                    while not (0 <= column <= 9):
+                        try:
+                            column = int(input("Now the column: "))
+                        except ValueError:
+                            pass
+
+                    if self.board[(row, column)] == 0:
+                        self.board[(row, column)] = 1
+                        occupied = True
+                    else:
+                        print("This tile is occupied by another ship."
+                              " You need to pick another one.\n")
+                orientation = input("Enter 'h' to place it horizontally,"
+                                    "or anything else to do it vertically.")
+                if orientation.lower() == 'h':
+                    pass
+                else:
+                    pass
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.set_players()
+    for player in game.players:
+        player.set_board()
